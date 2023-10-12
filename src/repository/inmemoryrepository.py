6d33c1e27c53943a9c864604601e0f8d8ee8a92e
PDFAtauchi <floresatauchi@gmail.com>
2023-10-12 17:41:12 -0500

    feat: inmemory repo with filters in list method

--- src/repository/inmemoryrepository.py
+++ src/repository/inmemoryrepository.py
@@ -5,5 +5,22 @@ class InMemoryRepository:
     def __init__(self, data):
         self.data = data
 
-    def list(self):
-        return [Room.from_dict(room) for room in self.data]
+    def list(self, filters=None):
+        result = [Room.from_dict(room) for room in self.data]
+
+        if filters is None:
+            return result
+
+        if "code__eq" in filters:
+            result = [r for r in result if r.code == filters["code__eq"]]
+
+        if "price__eq" in filters:
+            result = [r for r in result if r.code == filters["price__eq"]]
+
+        if "price__lt" in filters:
+            result = [r for r in result if r.code == filters["price__lt"]]
+
+        if "price__gt" in filters:
+            result = [r for r in result if r.code == filters["price__gt"]]
+
+        return result
